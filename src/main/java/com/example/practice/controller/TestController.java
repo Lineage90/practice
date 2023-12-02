1d44b57303d3ade0cffb8bc43b0a32027998ba4c
Laneige90 <kjw1243@naver.com>
2023-12-02 21:20:56 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/practice/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.practice.controller;
+
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello CICD!";
+    }
+}
