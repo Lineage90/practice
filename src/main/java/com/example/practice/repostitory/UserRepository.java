5c664ded61cea97464d186af36e939c689a8d946
Laneige90 <kjw1243@naver.com>
2023-12-07 19:18:51 +0900

    new

--- src/main/java/com/example/practice/repostitory/UserRepository.java
+++ src/main/java/com/example/practice/repostitory/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.practice.repostitory;
 
 import com.example.practice.Entity.User;
-import com.example.practice.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
